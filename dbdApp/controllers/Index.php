e18e16c68f20d48726c0f57bff49fd4e3fd51a5d
will <will@twilio.com>
2012-10-15 00:35:06 -0700

    move it up

--- dbdApp/controllers/Index.php
+++ dbdApp/controllers/Index.php
@@ -95,7 +95,7 @@ class Index extends GMController {
 			$this->getParam('imap-pass')
 		);
 
-		$dir = $this->getParam('dir') ?: DBD_APP_DIR . 'backups/' . $this->getParam('imap-user') . '/';
+		$dir = $this->getParam('dir') ?: realpath(DBD_APP_DIR . '../backups/') . $this->getParam('imap-user') . '/';
 		$limit = $this->getParam('limit') ?: -1;
 
 
